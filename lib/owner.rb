e337c33b94281467f758560f4cb33c61350ed09b
Alyssa Brown <alyssa.brown@live.com>
2019-02-13 03:27:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,9 +63,9 @@ class Owner
   end 
   
   def sell_pets 
-    pets.each do |pet, arr|
-      arr.map do |pet|
-        pet.mood = 'nervous'
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
       end
       arr.clear
     end
